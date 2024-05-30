09c6616a4fb98a86fd0dab9b92ce1b68262e17cb
yeqianmen <56306966+yeqianmen@users.noreply.github.com>
2024-05-30 15:15:53 +0800

    finish tag interface api

--- routers/router.go
+++ routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/yeqianmen/go-gin-example/pkg/setting"
+	"github.com/yeqianmen/go-gin-example/routers/api/v1"
 )
 
 func InitRouter() *gin.Engine {
@@ -10,10 +11,13 @@ func InitRouter() *gin.Engine {
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
 	gin.SetMode(setting.RunMode)
-	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "test",
-		})
-	})
+	apiv1 := r.Group("/api/v1")
+	{
+		apiv1.GET("/tags", v1.GetTags)
+		apiv1.POST("/tags", v1.AddTags)
+		apiv1.PUT("/tags/:id", v1.EditTags)
+		apiv1.DELETE("/tags/:id", v1.DeleteTags)
+
+	}
 	return r
 }
