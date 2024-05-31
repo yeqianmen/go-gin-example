38b3cb4d611bf4c846a7a6f4c66dabb2b5d021b5
yeqianmen <56306966+yeqianmen@users.noreply.github.com>
2024-05-31 14:49:41 +0800

    increase /article  interface

--- routers/router.go
+++ routers/router.go
@@ -18,6 +18,16 @@ func InitRouter() *gin.Engine {
 		apiv1.PUT("/tags/:id", v1.EditTags)
 		apiv1.DELETE("/tags/:id", v1.DeleteTags)
 
+		//获取文章列表
+		apiv1.GET("/articles", v1.GetArticles)
+		//获取指定文章
+		apiv1.GET("/articles/:id", v1.GetArticle)
+		//新建文章
+		apiv1.POST("/articles", v1.AddArticle)
+		//更新指定文章
+		apiv1.PUT("/articles/:id", v1.EditArticle)
+		//删除指定文章
+		apiv1.DELETE("/articles/:id", v1.DeleteArticle)
 	}
 	return r
 }
